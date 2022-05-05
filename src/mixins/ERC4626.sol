fa1ceb4e5ddce943a61108f82c4f343909c18fe9
Alexandru Beloiu <69057074+beloiual@users.noreply.github.com>
2022-05-05 16:27:31 -0500

    Update ERC4626.sol

--- src/mixins/ERC4626.sol
+++ src/mixins/ERC4626.sol
@@ -184,10 +184,13 @@ abstract contract ERC4626 is ERC20 {
     function afterDeposit(uint256 assets, uint256 shares) internal virtual {}
 }
 
-contract Vault is ERC4626 {
+contract ArborVault is ERC4626 {
+    using SafeTransferLib for ERC20;
+    using FixedPointMathLib for uint256;
 
-    // CONSTANTS
-    address constant MY_ACCOUNT = 0xc59E499d8E789986A08547ae5294D14C5dd91D9f;
+    /*//////////////////////////////////////////////////////////////
+                               IMMUTABLES
+    //////////////////////////////////////////////////////////////*/
 
     address constant USDC_ADDRESS = 0x02444D214962eC73ab733bB00Ca98879efAAa73d;
     ERC20 constant USDC_CONTRACT = ERC20(USDC_ADDRESS);
@@ -195,9 +198,11 @@ contract Vault is ERC4626 {
     address constant AAVE_POOL_ADDRESS = 0xC4744c984975ab7d41e0dF4B37E048Ef8006115E;
     Pool constant AAVE_POOL = Pool(AAVE_POOL_ADDRESS);
 
-    // FUNCTIONS
-
     constructor() ERC4626(USDC_CONTRACT, "Vault Shares", "VASH") {}
+
+    /*//////////////////////////////////////////////////////////////
+                            ACCOUNTING LOGIC
+    //////////////////////////////////////////////////////////////*/
 
     // ~20% of deposited USDC held in vault
     function reserve_assets() public view returns (uint256) {
@@ -221,6 +226,21 @@ contract Vault is ERC4626 {
     function totalAssets() public view override returns (uint256) {
         return reserve_assets() + collateral_assets();
     }
+
+    function previewWithdraw(uint256 assets) public view override returns (uint256) {
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+        uint original_return = supply == 0 ? assets : assets.mulDivUp(supply, totalAssets());
+
+        return assets < reserve_assets() ? original_return : reserve_assets();
+    }
+
+    function previewRedeem(uint256 shares) public view override returns (uint256) {
+        return previewWithdraw(convertToAssets(shares));
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        DEPOSIT/WITHDRAWAL LOGIC
+    //////////////////////////////////////////////////////////////*/
 
     function check_ratio() internal {
 
@@ -254,21 +274,118 @@ contract Vault is ERC4626 {
 
         check_ratio();
     }
+
+    /*//////////////////////////////////////////////////////////////
+                     DEPOSIT/WITHDRAWAL LIMIT LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    // quoted in usdc
+    function maxDeposit(address owner) public view override returns (uint256) {
+        return USDC_CONTRACT.balanceOf(owner);
+    }
+
+    // quoted in shares
+    function maxMint(address owner) public view override returns (uint256) {
+        return convertToShares(maxDeposit(owner));
+    }
+
+    function min(uint x, uint y) internal view returns (uint) {
+        return x < y ? x : y;
+    }
+
+    // quoted in usdc
+    function maxWithdraw(address owner) public view override returns (uint256) {
+        uint owner_assets = convertToAssets(balanceOf[owner]);
+        return min(owner_assets, reserve_assets());
+    }
+
+    // quoted in shares
+    function maxRedeem(address owner) public view override returns (uint256) {
+        return convertToShares(maxWithdraw(owner));
+    }
 }
 
-contract MintableERC20 is ERC20("USD Coin", "USDC", 6) {
-    function mint() public {
-        uint amount = 1_000_000;
-        address to = msg.sender;
+contract VaultTest {
+    // arbor vault refers to the contract above, that Alex and I wrote
+    ArborVault VAULT_CONTRACT;
 
-        totalSupply += amount;
-
-        // Cannot overflow because the sum of all user
-        // balances can't exceed the max uint256 value.
-        unchecked {
-            balanceOf[to] += amount;
-        }
-
-        emit Transfer(address(0), to, amount);
+    address constant USDC_ADDRESS = 0x02444D214962eC73ab733bB00Ca98879efAAa73d;
+    ERC20 constant USDC_CONTRACT = ERC20(USDC_ADDRESS);
+    address VAULT_ADDRESS;
+    // deploy new ArborVault (everything starts at 0) and pass its address into VaultTest's constructor
+    constructor(address input_VAULT_ADDRESS) public {
+        VAULT_CONTRACT = ArborVault(input_VAULT_ADDRESS);
+        VAULT_ADDRESS = input_VAULT_ADDRESS;
     }
+
+    // call this first
+    function testBoot() public {
+        //doing functions in quick succession so assuming no interests
+        require(
+           (VAULT_CONTRACT.reserve_assets() == 0) && VAULT_CONTRACT.collateral_assets() == 0 && VAULT_CONTRACT.totalAssets() == 0,
+            "assets don't start at 0"
+        );
+
+    }
+    
+    // call this second
+     function testDeposit() public {
+         USDC_CONTRACT.approve(VAULT_ADDRESS, 20);
+       VAULT_CONTRACT.deposit(20);
+        /*require(
+            VAULT_CONTRACT.reserve_assets() == 2  && VAULT_CONTRACT.collateral_assets() == 8 && VAULT_CONTRACT.totalAssets() == 10,
+            "assets not added properly"
+        );
+        
+        require(
+            VAULT_CONTRACT.maxWithdraw(address(this)) == 2,
+            "maxWithdraw incorrect"
+        );
+        require(
+            VAULT_CONTRACT.maxRedeem(address(this)) == 2,
+            "maxRedeem incorrect"
+        );
+        USDC_CONTRACT.approve(VAULT_ADDRESS, 10);
+        VAULT_CONTRACT.deposit(10);*/
+        require(
+            VAULT_CONTRACT.maxWithdraw(address(this)) == 4,
+            "maxWithdraw incorrect"
+        );
+        require(
+            VAULT_CONTRACT.maxRedeem(address(this)) == 4,
+            "maxRedeem incorrect"
+        );
+        require(
+            VAULT_CONTRACT.reserve_assets() == 4  && VAULT_CONTRACT.collateral_assets() == 16 && VAULT_CONTRACT.totalAssets() == 20,
+            "assets not added properly"
+        );
+        require(
+            VAULT_CONTRACT.maxDeposit(address(this)) >= 0 && (VAULT_CONTRACT.maxDeposit(address(this)) == VAULT_CONTRACT.maxMint(address(this))),
+            "maxDeposit and maxMint incorrect"
+        );
+        
+        
+
+
+     }
+
+    // call this third
+     function testWithdraw() public {
+        VAULT_CONTRACT.withdraw(1);
+        require(
+            VAULT_CONTRACT.reserve_assets() == 3  && VAULT_CONTRACT.collateral_assets() == 16 && VAULT_CONTRACT.totalAssets() == 19,
+            "assets not removed properly1"
+
+        );
+        VAULT_CONTRACT.withdraw(1);
+        require(
+            VAULT_CONTRACT.reserve_assets() == 3  && VAULT_CONTRACT.collateral_assets() == 15 && VAULT_CONTRACT.totalAssets() == 18,
+            "assets not removed properly2"
+        );
+        VAULT_CONTRACT.withdraw(VAULT_CONTRACT.maxWithdraw(address(this)));
+        require(
+            VAULT_CONTRACT.reserve_assets() == 3  && VAULT_CONTRACT.collateral_assets() == 12 && VAULT_CONTRACT.totalAssets() == 15,
+            "assets not removed properly (maxWithdraw)"
+        );
+     }
 }
